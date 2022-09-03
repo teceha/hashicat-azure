0aef8f734c98840cff875ff40fc76f11fe16cfe3
Tomasz Charatonik <teceha@gmail.com>
2022-09-03 17:26:22 +0000

    changed resource group name

--- network.tf
+++ network.tf
@@ -2,5 +2,5 @@ module "network" {
   source  = "Azure/network/azurerm"
   version = "3.5.0"
   # insert the 1 required variable here
-  resource_group_name   = "myresourcegroup"
+  resource_group_name   = "${var.prefix}-workshop"
 }
